37ae3fb906134d232180395041a6d4cd609ece8c
Ozgur Yigit <ozgurcanyigit@MacBook-Air.local>
2018-10-21 11:18:39 +0300

    add more robust DB add query,
    Initialize product sales and mapping data with csv files

--- populate_database.R
+++ populate_database.R
@@ -7,24 +7,26 @@ composeUserAddQuery <- function(user, password, email, role, department)
     sprintf("INSERT INTO pw VALUES('%s', '%s', '%s', '%s', '%s')", user, ., email, role, department)
 }
 
-composeGeneralAddQuery <- function(x , ...) {
+GeneralDBAddQuery <- function(x , y, ...) {
   
   variable.list <- list(...)
   var.count <- length(variable.list)
   
-  text <- paste0("INSERT INTO ",x, " VALUES(")
+  init <- paste0("INSERT INTO ",x, " VALUES(")
 
-  product.list <- character(0)
+  for(j in 1:nrow(y)){
+  paste.list <- character(0)
   for(i in 1:var.count) {
-    temp <- paste0("'",variable.list[i],"',")
+    temp <- paste0("'",y[j,i],"',")
     if(i == var.count) {
-      temp <- paste0("'",variable.list[i],"'")
+      temp <- paste0("'",y[j,i],"'")
     }
-    product.list <- paste0(product.list, temp)
+    paste.list <- paste0(paste.list, temp)
   }
-  text <- paste0(text, product.list, ")")
+  text <- paste0(init, paste.list, ")")
   dbSendQuery(db, text) %>%
     dbClearResult()
+  }
 }
 
 
@@ -40,11 +42,13 @@ sendUserAddQuery <- function(user, password, email, role, department)
 # initialize a connection, will create a db if not exists
 db <- dbConnect(SQLite(), dbname = database)
 
-# create the table for the logins
+# create the table for the logins, user_product_mappings and product sales
 dbClearResult(dbSendQuery(db, 'DROP TABLE IF EXISTS pw'))
 dbClearResult(dbSendQuery(db, 'DROP TABLE IF EXISTS user_products'))
+dbClearResult(dbSendQuery(db, 'DROP TABLE IF EXISTS product_sales'))
 dbClearResult(dbSendQuery(db, 'CREATE TABLE pw (user TEXT, password TEXT, email TEXT, role TEXT, department TEXT)'))
 dbClearResult(dbSendQuery(db, 'CREATE TABLE user_products (user TEXT, product TEXT)'))
+dbClearResult(dbSendQuery(db, 'CREATE TABLE product_sales (product TEXT, year TEXT, sales NUMBER)'))
 
 # initialize a DT of some dummy logins
 db_logins <- data.table::data.table(
@@ -57,16 +61,13 @@ db_logins[, email := paste0(user, "@ilacfirmasi.com")]
 
 # Initialize another DT of product - user mappings
 
-initial.product.list <- c('A', 'B', 'C', 'D', 'E')
-
-db_products <- data.table::data.table(
-  user = c(rep('Mert', 5), rep('Ozgur', 5), rep('Senay', 3), rep('Merve', 2)),
-  product = c(initial.product.list, initial.product.list, 'A', 'B', 'C', 'D', 'E')
-)
+db_products <- read.csv("initial_productsales_db.csv", stringsAsFactors = FALSE)
+db_product_sales <- read.csv("initial_sales_db.csv", stringsAsFactors = FALSE)
 
 # perform additions
 login.success <- db_logins[, mapply(sendUserAddQuery, user, password, email, role, department)]
-userproduct.success <- db_products[, mapply(composeGeneralAddQuery, 'user_products', user, product)]
+GeneralDBAddQuery('product_sales', db_product_sales, product, year, sales)
+GeneralDBAddQuery('user_products', db_products, user, product)
 
 
 # check that all are TRUE
